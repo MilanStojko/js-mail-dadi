3ca5b3612064e59acca3fd3e9518fff531320b72
Milan Stojko <milanmilance2000.ms@gmail.com>
2021-12-13 17:08:50 +0100

    mail done

---
+++ mail/myscript.js
@@ -0,0 +1,24 @@
+
+let gruppoMail = ["milan@gmail.com", "matteo@gmail.com" , "lorenzo@gmail.com"];
+let myMail = prompt ("inserisci la tua mail");
+let emailCheck = false;
+
+for(let i= 0; i < gruppoMail.length; i++){
+    if(myMail == gruppoMail[i]){
+        emailCheck = true;
+    }
+}
+
+if(emailCheck){
+    alert("email corretta");
+}
+else{
+    alert("email errata");
+}
+
+if(emailCheck){
+    console.log("email corretta");
+}
+else{
+    console.log("email errata");
+}
